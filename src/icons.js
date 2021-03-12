b260443221f619531211e6c08b951b005aa2e4f6
huangzx <2511646043@qq.com>
2021-03-12 12:53:32 +0800

    update 增加表格分页中需要用到的LeftIcon和RightIcon

--- src/icons.js
+++ src/icons.js
@@ -4,3 +4,5 @@ export { default as DashboardOutline } from "@ant-design/icons/lib/outline/Dashb
 export { default as FormOutline } from "@ant-design/icons/lib/outline/FormOutline";
 export { default as GlobalOutline } from "@ant-design/icons/lib/outline/GlobalOutline";
 export { default as MenuOutline } from "@ant-design/icons/lib/outline/MenuOutline";
+export { default as LeftOutline } from "@ant-design/icons/lib/outline/LeftOutline";
+export { default as RightOutline } from "@ant-design/icons/lib/outline/RightOutline";
