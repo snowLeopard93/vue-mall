e2b0c3ba2c22d141ec64163222d2bf70daca6269
huangzx <2511646043@qq.com>
2021-03-14 19:09:39 +0800

    add 增加搜索框布局

---
+++ src/layouts/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="searchBar">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar"
+};
+</script>
+
+<style scoped>
+.searchBar {
+  height: 50px;
+  line-height: 50px;
+  margin-bottom: 10px;
+}
+</style>
