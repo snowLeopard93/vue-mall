edf7ee4dd7a16aa274aded33f73afefe3bbe67fb
huangzx <2511646043@qq.com>
2021-03-20 17:26:40 +0800

    update 对MyToolBar组件进行封装，增加默认的刷新按钮、文本查询框和查询按钮

--- src/layouts/ToolBar/index.vue
+++ src/layouts/ToolBar/index.vue
@@ -1,17 +1,50 @@
 <template>
   <div class="toolBar">
     <div class="toolBar-left">
-      <slot name="leftToolBar"></slot>
+      <slot name="leftToolBar">
+        <a-button type="primary" @click="refreshBtnClick">
+          <IconFont type="icon-refresh" style="font-size: 14px;" />
+        </a-button>
+      </slot>
     </div>
     <div class="toolBar-right">
-      <slot name="rightToolBar"></slot>
+      <slot name="rightToolBar">
+        <div style="width: 200px;display: inline-block;margin-right: 10px;">
+          <a-input
+            placeholder="请输入"
+            v-model="searchParamName"
+            @pressEnter="changeSearchParamName"
+          />
+        </div>
+        <a-button type="primary" @click="searchBtnClick">
+          <a-icon type="search" />
+        </a-button>
+      </slot>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MyToolBar"
+  name: "MyToolBar",
+  data() {
+    return {
+      searchParamName: ""
+    };
+  },
+  methods: {
+    changeSearchParamName() {
+      const value = this.searchParamName;
+      this.$emit("handleChangeSearchParamName", value);
+    },
+    refreshBtnClick() {
+      this.$emit("handleRefresh");
+    },
+    searchBtnClick() {
+      const value = this.searchParamName;
+      this.$emit("handleSearchBtnClick", value);
+    }
+  }
 };
 </script>
 
