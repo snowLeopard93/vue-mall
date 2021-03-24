c638ee48410a5f63140ed8def1234ba62563318f
huangzx <2511646043@qq.com>
2021-03-24 21:19:19 +0800

    update 增加双击表格行时右侧弹窗显示详情

---
+++ src/views/System/User/detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <DetailDrawer>
+      <template v-slot:content>
+        aaaaaa
+      </template>
+    </DetailDrawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserDetail",
+  methods: {}
+};
+</script>
+
+<style scoped></style>
