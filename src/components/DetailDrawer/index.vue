c47a94d18fc4cb4bfd6d7dca7481a6a9c1ae6107
huangzx <2511646043@qq.com>
2021-03-24 21:15:54 +0800

    add 引入基于ant-design-vue Drawer组件封装的 DetailDrawer组件，用于显示详情界面

---
+++ src/components/DetailDrawer/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <a-drawer
+      title="Create a new account"
+      :width="720"
+      :visible="detailDrawerVisible"
+      :body-style="{ paddingBottom: '80px' }"
+      @close="onClose"
+    >
+      <slot name="content"> </slot>
+      <div
+        :style="{
+          position: 'absolute',
+          right: 0,
+          bottom: 0,
+          width: '100%',
+          borderTop: '1px solid #e9e9e9',
+          padding: '10px 16px',
+          background: '#fff',
+          textAlign: 'right',
+          zIndex: 1
+        }"
+      ></div>
+    </a-drawer>
+  </div>
+</template>
+<script>
+export default {
+  name: "DetailDrawer",
+  computed: {
+    detailDrawerVisible() {
+      return this.$store.getters.detailDrawerVisible;
+    }
+  },
+  methods: {
+    onClose() {
+      console.log("detailDrawerClose");
+      this.$store.commit("changeDetailDrawerVisible", false);
+    }
+  }
+};
+</script>
+<style scoped lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -29,6 +29,7 @@ import queryString from "query-string";
 import MyTable from "./components/Table";
 import MySelect from "./components/Select";
 import MyToolBar from "./layouts/ToolBar";
+import DetailDrawer from "./components/DetailDrawer";
 
 Vue.config.productionTip = false;
 
@@ -60,6 +61,7 @@ Vue.component("IconFont", IconFont);
 Vue.component("MyTable", MyTable);
 Vue.component("MySelect", MySelect);
 Vue.component("MyToolBar", MyToolBar);
+Vue.component("DetailDrawer", DetailDrawer);
 
 const i18n = new VueI18n({
   locale: queryString.parse(location.search).locale || "zhCN",
