568cdc7a300758d8ccf3298d996fe3c8fb2fcf9f
huangzx <2511646043@qq.com>
2021-04-02 08:50:00 +0800

    update 修改新增用户表单

--- src/views/System/User/modify.vue
+++ src/views/System/User/modify.vue
@@ -1,11 +1,93 @@
 <template>
   <div>
-    <MyModifyDrawer :title="title" :ruleForm="ruleForm" :rules="rules">
+    <MyModifyDrawer :title="title" @submit="submitForm">
+      <template v-slot:content>
+        <a-form-model ref="ruleForm" :model="ruleForm" :rules="rules">
+          <a-row :gutter="16">
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.loginNameLabel']"
+                prop="loginName"
+              >
+                <a-input v-model="ruleForm.loginName" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.userNameLabel']"
+                prop="userName"
+              >
+                <a-input v-model="ruleForm.userName" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="16">
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.emailLabel']"
+                prop="email"
+              >
+                <a-input v-model="ruleForm.email" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.telPhoneLabel']"
+                prop="telPhone"
+              >
+                <a-input v-model="ruleForm.telPhone" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="16">
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.statusLabel']"
+                prop="status"
+              >
+                <a-input v-model="ruleForm.status" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.addressLabel']"
+                prop="address"
+              >
+                <a-input v-model="ruleForm.address" autocomplete="off" />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="16">
+            <a-col :span="24">
+              <a-form-model-item
+                has-feedback
+                :label="$t('message')['app.system.user.remarkLabel']"
+                prop="remark"
+              >
+                <a-textarea
+                  v-model="ruleForm.remark"
+                  autocomplete="off"
+                  :rows="4"
+                />
+              </a-form-model-item>
+            </a-col>
+          </a-row>
+        </a-form-model>
+      </template>
     </MyModifyDrawer>
   </div>
 </template>
 
 <script>
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
+
 export default {
   name: "UserModify",
   data() {
@@ -55,7 +137,25 @@ export default {
       }
     };
   },
-  methods: {}
+  methods: {
+    submitForm() {
+      this.$refs["ruleForm"].validate(valid => {
+        if (valid) {
+          console.log(this.ruleForm);
+          let values = this.ruleForm;
+          let key = uuidv4();
+          let createTime = moment().format("YYYY-MM-DD HH:mm:ss");
+          values["key"] = key;
+          values["createTime"] = createTime;
+          this.$store.dispatch("user/addUser", values);
+          this.$store.commit("system/changeModifyDrawerVisible", false);
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
 };
 </script>
 
