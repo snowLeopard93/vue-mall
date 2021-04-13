d7000ffc336f149706b0fae86818969da01f2c9e
huangzx <2511646043@qq.com>
2021-04-13 13:56:10 +0800

    update 删除无用代码

--- src/views/System/Notice/preview.vue
+++ src/views/System/Notice/preview.vue
@@ -5,24 +5,6 @@
         <div v-html="notice.content"></div>
       </template>
     </MyDialog>
-    <!-- <MyDetailDrawer :title="title">
-      <template v-slot:content>
-        <div class="noticeDetail-box">
-          <div class="noticeDetail-label">标题：</div>
-          <div class="noticeDetail-detail">{{ notice.noticeTitle }}</div>
-          <div class="noticeDetail-label">状态：</div>
-          <div class="noticeDetail-detail">{{ notice.showStatus }}</div>
-          <div class="noticeDetail-label">创建时间：</div>
-          <div class="noticeDetail-detail">{{ notice.ctime }}</div>
-          <div class="noticeDetail-label">修改时间：</div>
-          <div class="noticeDetail-detail">{{ notice.mtime }}</div>
-        </div>
-        <div class="noticeDetail-long-box">
-          <div class="noticeDetail-label">内容：</div>
-          <div class="noticeDetail-long-detail">{{ notice.content }}</div>
-        </div>
-      </template>
-    </MyDetailDrawer> -->
   </div>
 </template>
 
