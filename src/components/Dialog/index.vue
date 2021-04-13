3539f2fa2ac2e26cddb03ec37a283468ff7abaee
huangzx <2511646043@qq.com>
2021-04-13 13:51:35 +0800

    add 增加基于ant-design-vue Modal组件封装的 MyDialog 组件

---
+++ src/components/Dialog/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <a-modal
+      :visible="visible"
+      :title="title"
+      :footer="null"
+      @ok="onSubmit"
+      @cancel="onClose"
+    >
+      <slot name="content"></slot>
+    </a-modal>
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "MyDialog",
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: () => {}
+    }
+  },
+  computed: mapState({
+    visible: state => state.system.dialogVisible
+  }),
+  methods: {
+    onSubmit() {
+      this.$store.commit("system/changeDialogVisible", false);
+    },
+    onClose() {
+      this.$store.commit("system/changeDialogVisible", false);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
