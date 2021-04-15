b669811f6a10c73460a2525e005fbc4ccd34390a
huangzx <2511646043@qq.com>
2021-04-15 11:44:56 +0800

    add 增加基于ant-design-vue Modal组件封装的 MyConfirmDialog组件

---
+++ src/components/Dialog/ConfirmDialog/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { mapState } from "vuex";
+import { Modal } from "ant-design-vue";
+
+export default {
+  name: "MyConfirmDialog",
+  props: {
+    content: {
+      type: String,
+      required: true,
+      default: () => {}
+    }
+  },
+  computed: mapState({
+    visible: state => state.system.confirmDialogVisible
+  }),
+  watch: {
+    visible(val) {
+      if (val) {
+        this.$nextTick(() => {
+          this.showConfirm();
+        });
+      }
+    }
+  },
+  methods: {
+    showConfirm() {
+      let that = this;
+      console.log(Modal);
+      Modal.confirm({
+        title: that.content,
+        onOk() {
+          that.$emit("handleConfirmClick");
+        },
+        onCancel() {}
+      });
+    }
+  }
+};
+</script>
