4855f89d5a79a618ea7dfb41992ebf5fe026acec
huangzx <2511646043@qq.com>
2021-05-30 10:00:43 +0800

    add 增加工作台模块的路由配置文件

--- src/router/index.js
+++ src/router/index.js
@@ -10,131 +10,132 @@ import { notification } from "ant-design-vue";
 
 import systemRoutes from "./system";
 import orderManageRoutes from "./orderManage";
+import workbenchRoutes from "./workbench";
 
 Vue.use(VueRouter);
 
 let defaultRoutes = [
-  {
-    path: "/user",
-    hideInMenu: true,
-    component: () =>
-      import(/* webpackChunkName: "layout" */ "../layouts/UserLayout"),
-    children: [
-      {
-        path: "/user",
-        redirect: "/user/login"
-      },
-      {
-        path: "/user/login",
-        name: "login",
-        component: () =>
-          import(/* webpackChunkName: "user" */ "../views/User/Login")
-      },
-      {
-        path: "/user/register",
-        name: "register",
-        component: () =>
-          import(/* webpackChunkName: "register" */ "../views/User/Register")
-      }
-    ]
-  },
-  {
-    path: "/",
-    meta: { authority: ["user", "admin"] },
-    component: () =>
-      import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout"),
-    children: [
-      {
-        path: "/",
-        redirect: "/dashboard/analysis"
-      },
-      {
-        path: "/dashboard",
-        name: "dashboard",
-        meta: {
-          icon: "dashboard",
-          title: "仪表盘"
-        },
-        component: { render: h => h("router-view") },
-        children: [
-          {
-            path: "/dashboard/analysis",
-            name: "analysis",
-            meta: {
-              title: "分析页"
-            },
-            component: () =>
-              import(
-                /* webpackChunkName: "analysis" */ "../views/Dashboard/Analysis"
-              )
-          }
-        ]
-      },
-      {
-        path: "/form",
-        name: "form",
-        meta: {
-          icon: "form",
-          title: "表单",
-          authority: ["admin"]
-        },
-        component: { render: h => h("router-view") },
-        children: [
-          {
-            path: "/form/basic-form",
-            name: "basicForm",
-            meta: {
-              title: "基础表单"
-            },
-            component: () =>
-              import(/* webpackChunkName: "form" */ "../views/Forms/BasicForm")
-          },
-          {
-            path: "/form/step-form",
-            name: "stepForm",
-            meta: {
-              title: "分步表单"
-            },
-            hideChildrenInMenu: true,
-            component: () =>
-              import(
-                /* webpackChunkName: "form" */ "../views/Forms/StepForm/index"
-              ),
-            children: [
-              {
-                path: "/form/step-form",
-                redirect: "/form/step-form/info"
-              },
-              {
-                path: "/form/step-form/info",
-                name: "info",
-                component: () =>
-                  import(
-                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step1"
-                  )
-              },
-              {
-                path: "/form/step-form/confirm",
-                name: "confirm",
-                component: () =>
-                  import(
-                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step2"
-                  )
-              },
-              {
-                path: "/form/step-form/result",
-                name: "result",
-                component: () =>
-                  import(
-                    /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step3"
-                  )
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  },
+  // {
+  //   path: "/user",
+  //   hideInMenu: true,
+  //   component: () =>
+  //     import(/* webpackChunkName: "layout" */ "../layouts/UserLayout"),
+  //   children: [
+  //     {
+  //       path: "/user",
+  //       redirect: "/user/login"
+  //     },
+  //     {
+  //       path: "/user/login",
+  //       name: "login",
+  //       component: () =>
+  //         import(/* webpackChunkName: "user" */ "../views/User/Login")
+  //     },
+  //     {
+  //       path: "/user/register",
+  //       name: "register",
+  //       component: () =>
+  //         import(/* webpackChunkName: "register" */ "../views/User/Register")
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: "/",
+  //   meta: { authority: ["user", "admin"] },
+  //   component: () =>
+  //     import(/* webpackChunkName: "layout" */ "../layouts/BasicLayout"),
+  //   children: [
+  //     {
+  //       path: "/",
+  //       redirect: "/dashboard/analysis"
+  //     },
+  //     {
+  //       path: "/dashboard",
+  //       name: "dashboard",
+  //       meta: {
+  //         icon: "dashboard",
+  //         title: "仪表盘"
+  //       },
+  //       component: { render: h => h("router-view") },
+  //       children: [
+  //         {
+  //           path: "/dashboard/analysis",
+  //           name: "analysis",
+  //           meta: {
+  //             title: "分析页"
+  //           },
+  //           component: () =>
+  //             import(
+  //               /* webpackChunkName: "analysis" */ "../views/Dashboard/Analysis"
+  //             )
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: "/form",
+  //       name: "form",
+  //       meta: {
+  //         icon: "form",
+  //         title: "表单",
+  //         authority: ["admin"]
+  //       },
+  //       component: { render: h => h("router-view") },
+  //       children: [
+  //         {
+  //           path: "/form/basic-form",
+  //           name: "basicForm",
+  //           meta: {
+  //             title: "基础表单"
+  //           },
+  //           component: () =>
+  //             import(/* webpackChunkName: "form" */ "../views/Forms/BasicForm")
+  //         },
+  //         {
+  //           path: "/form/step-form",
+  //           name: "stepForm",
+  //           meta: {
+  //             title: "分步表单"
+  //           },
+  //           hideChildrenInMenu: true,
+  //           component: () =>
+  //             import(
+  //               /* webpackChunkName: "form" */ "../views/Forms/StepForm/index"
+  //             ),
+  //           children: [
+  //             {
+  //               path: "/form/step-form",
+  //               redirect: "/form/step-form/info"
+  //             },
+  //             {
+  //               path: "/form/step-form/info",
+  //               name: "info",
+  //               component: () =>
+  //                 import(
+  //                   /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step1"
+  //                 )
+  //             },
+  //             {
+  //               path: "/form/step-form/confirm",
+  //               name: "confirm",
+  //               component: () =>
+  //                 import(
+  //                   /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step2"
+  //                 )
+  //             },
+  //             {
+  //               path: "/form/step-form/result",
+  //               name: "result",
+  //               component: () =>
+  //                 import(
+  //                   /* webpackChunkName: "form" */ "../views/Forms/StepForm/Step3"
+  //                 )
+  //             }
+  //           ]
+  //         }
+  //       ]
+  //     }
+  //   ]
+  // },
   {
     path: "/403",
     name: "403",
@@ -155,7 +156,12 @@ let defaultRoutes = [
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [...defaultRoutes, ...systemRoutes, ...orderManageRoutes]
+  routes: [
+    ...workbenchRoutes,
+    ...systemRoutes,
+    ...orderManageRoutes,
+    ...defaultRoutes
+  ]
 });
 
 router.beforeEach((to, from, next) => {
